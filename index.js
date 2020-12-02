79e83acb2b21e53030877a025ec519719d0695ec
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-01 17:04:12 -0800

    created axes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let url =
 let req = new XMLHttpRequest();
 
 var baseTemp,
-  values,
+  values = [],
   xScale,
   yScale,
   width = 1200,
@@ -16,16 +16,35 @@ const drawCanvas = () => {
   canvas.attr("width", width).attr("height", height);
 };
 
-const generateScales = () => {};
+const generateScales = () => {
+  xScale = d3
+    .scaleLinear()
+    .domain([1752, 2016])
+    .range([padding, width - padding]);
+  yScale = d3.scaleTime().range([padding, height - padding]);
+};
 
 const drawCells = () => {};
 
-const drawAxes = () => {};
+const drawAxes = () => {
+  const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+  const yAxis = d3.axisLeft(yScale);
+
+  canvas
+    .append("g")
+    .call(xAxis)
+    .attr("id", "x-axis")
+    .attr("transform", `translate(0, ${height - padding})`);
+
+  canvas.append("g").call(yAxis).attr("id", "y-axis").attr("transform", `translate(${padding}, 0)`);
+};
 
 req.open("GET", url, true);
 req.onload = () => {
-  values = JSON.parse(req.responseText);
-  console.log(values)
+  let object = JSON.parse(req.responseText);
+  values = object.monthlyVariance;
+  baseTemp = object.baseTemperature;
+  console.log(object);
   drawCanvas();
   generateScales();
   drawCells();
